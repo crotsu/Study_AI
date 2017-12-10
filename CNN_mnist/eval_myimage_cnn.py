7ac3dd87c037e29c0bcebb79916e10c3b9f860da
crotsu <crotsu@gmail.com>
2017-12-10 21:53:07 +0900

    CNN_mnistを追加

---
+++ CNN_mnist/eval_myimage_cnn.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import argparse
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import chainer.initializers as I
+from chainer import training
+from chainer.training import extensions
+from PIL import Image
+import numpy as np
+
+class MLP(chainer.Chain):
+    def __init__(self, n_units, n_out):
+        w = I.Normal(scale=0.05) # モデルパラメータの初期化
+        super(MLP, self).__init__(
+            conv1=L.Convolution2D(1, 16, 5, 1, 0), # 1層目の畳み込み層（フィルタ数は16）
+            conv2=L.Convolution2D(16, 32, 5, 1, 0), # 2層目の畳み込み層（フィルタ数は32）
+            l3=L.Linear(None, n_out, initialW=w), #クラス分類用
+        )
+    def __call__(self, x):
+        h1 = F.max_pooling_2d(F.relu(self.conv1(x)), ksize=2, stride=2) # 最大値プーリングは2×2，活性化関数はReLU
+        h2 = F.max_pooling_2d(F.relu(self.conv2(h1)), ksize=2, stride=2) 
+        y = self.l3(h2)
+        return y
+"""
+自分で用意した手書き文字画像をモデルに合うように変換する処理
+"""
+def convert_cnn(img):    
+    data = np.array(Image.open(img).convert('L').resize((28, 28)), dtype=np.float32)  # ファイルを読込み，リサイズして配列に変換        
+    data = (255.0 - data) / 255.0 # 白黒反転して正規化
+    data = data.reshape(1, 1, 28, 28) # データの形状を変更
+    return data
+
+def main():
+    # オプション処理
+    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
+    parser.add_argument('--inputimage', '-i', default='',
+                        help='入力画像ファイル')    
+    parser.add_argument('--model', '-m', default='',
+                        help='CNNモデルで評価する')
+    parser.add_argument('--unit', '-u', type=int, default=1000,
+                        help='Number of units')    
+    args = parser.parse_args()
+
+    print('自分の手書き文字を学習したモデルで評価してみるプログラム')
+    print('# 入力画像ファイル: {}'.format(args.inputimage))
+    print('# 学習済みモデルファイル: {}'.format(args.model))
+    print('')
+
+    # モデルのインスタンス作成    
+    model = L.Classifier(MLP(args.unit, 10))    
+    # モデルの読み込み
+    chainer.serializers.load_npz(args.model, model)
+
+    # 入力画像を28x28のグレースケールデータ（0〜1に正規化）に変換する
+    img = convert_cnn(args.inputimage)
+    x = chainer.Variable(np.asarray(img)) # 配列データをchainerで扱う型に変換
+    
+    y = model.predictor(x) # フォワード
+    c = F.softmax(y).data.argmax()    
+    print('判定結果は{}です。'.format(c))        
+
+if __name__ == '__main__':
+    main()
---
+++ CNN_mnist/train_mnist_cnn.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+from __future__ import print_function
+import argparse
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import chainer.initializers as I
+from chainer import training
+from chainer.training import extensions
+
+class MLP(chainer.Chain):
+    def __init__(self, n_units, n_out):
+        w = I.Normal(scale=0.05) # モデルパラメータの初期化
+        super(MLP, self).__init__(
+            conv1=L.Convolution2D(1, 16, 5, 1, 0), # 1層目の畳み込み層（フィルタ数は16）
+            conv2=L.Convolution2D(16, 32, 5, 1, 0), # 2層目の畳み込み層（フィルタ数は32）
+            l3=L.Linear(None, n_out, initialW=w), #クラス分類用
+        )
+    def __call__(self, x):
+        h1 = F.max_pooling_2d(F.relu(self.conv1(x)), ksize=2, stride=2) # 最大値プーリングは2×2，活性化関数はReLU
+        h2 = F.max_pooling_2d(F.relu(self.conv2(h1)), ksize=2, stride=2) 
+        y = self.l3(h2)
+        return y
+
+def main():
+    parser = argparse.ArgumentParser(description='Chainer example: MNIST')
+    parser.add_argument('--batchsize', '-b', type=int, default=100, help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result', help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='', help='Resume the training from snapshot')
+    parser.add_argument('--unit', '-u', type=int, default=1000, help='Number of units')
+    args = parser.parse_args()
+
+    print('GPU: {}'.format(args.gpu))
+    print('# unit: {}'.format(args.unit))
+    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('# epoch: {}'.format(args.epoch))
+    print('')
+
+    train, test = chainer.datasets.get_mnist(ndim=3) # ndim=3を引数で与えるだけでOK
+    model = L.Classifier(MLP(args.unit, 10), lossfun=F.softmax_cross_entropy)
+    if args.gpu >= 0:
+        chainer.cuda.get_device(args.gpu).use()
+        model.to_gpu()
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(model)
+    train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize, repeat=False, shuffle=False)
+    updater = training.StandardUpdater(train_iter, optimizer, device=args.gpu)
+
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
+    trainer.extend(extensions.Evaluator(test_iter, model, device=args.gpu))
+    trainer.extend(extensions.dump_graph('main/loss'))
+    trainer.extend(extensions.snapshot(), trigger=(args.epoch, 'epoch'))
+    trainer.extend(extensions.LogReport())
+    trainer.extend( extensions.PlotReport(['main/loss', 'validation/main/loss'], 'epoch', file_name='loss.png'))
+    trainer.extend( extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], 'epoch', file_name='accuracy.png'))
+    trainer.extend(extensions.PrintReport( ['epoch', 'main/loss', 'validation/main/loss', 'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))
+    trainer.extend(extensions.ProgressBar())
+
+    if args.resume:
+        chainer.serializers.load_npz(args.resume, trainer)
+
+    trainer.run()
+    model.to_cpu()
+
+    modelname = args.out + "/MLP.model"
+    print('save the trained model: {}'.format(modelname))
+    chainer.serializers.save_npz(modelname, model)
+
+if __name__ == '__main__':
+    main()
